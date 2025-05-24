34001ebedefeb18216861ac1b78c50e7a4edc315
Jassyq <jiq004@ucsd.edu>
2025-05-23 23:26:01 -0700

    updated link

--- global.js
+++ global.js
@@ -94,14 +94,20 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
   containerElement.innerHTML = '';
   projects.forEach((project) => {
     const article = document.createElement('article');
+    let url = project.url;
+    if (url && !/^https?:\/\//i.test(url)) {
+      url = 'https://' + url;
+    }
+    let titleHtml = url
+      ? `<a href="${url}" target="_blank" class="project-link"><h3>${project.title}</h3></a>`
+      : `<h3>${project.title}</h3>`;
     article.innerHTML = `
-    <h3>${project.title}</h3>
+    ${titleHtml}
     <p class="project-year">c. ${project.year}</p>
     <img src="${project.image}" alt="${project.title}">
     <p>${project.description}</p>
-    
-`;
-  containerElement.appendChild(article);
+    `;
+    containerElement.appendChild(article);
   });
 }
 
